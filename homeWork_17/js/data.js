c4c206031c4843b8cd626c454beb436252d18687
Igor Valnev <dx2389@gmail.com>
2019-03-30 13:48:01 +0300

    Сделал первые задачи + добавил второе большое задание.

---
+++ homeWork_17/js/data.js
@@ -0,0 +1,102 @@
+// Это реальный объект на основе которого вам надо дудет строить галлерею
+var data = [{
+	url: "desktopwallpapers.org.ua/mini/201507/40069.jpg",
+	name: "CHEVROLET",
+	id : 1,
+	params: {
+		status: true,
+		progress: "80"
+	},
+	description : "Be conveyed to users of assistive technologies – such as",
+	date : 1422153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40068.jpg",
+	name: "DEWOO",
+	id : 2,
+	params: {
+		status: true,
+		progress: "88"
+	},
+	description : "sing color to add meaning to a button",
+	date : 1421153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40067.jpg",
+	name: "FOLKSWAGEN",
+	id : 3,
+	params: {
+		status: false,
+		progress: "64"
+	},
+	description : "be conveyed to users of assistive technologies",
+	date : 1426153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40057.jpg",
+	name: "FERRARI",
+	id : 4,
+	params: {
+		status: true,
+		progress: "38"
+	},
+	description : "ssistive technologies – such as screen readers. Ensure",
+	date : 1428153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40066.jpg",
+	name: "BMW",
+	id : 5,
+	params: {
+		status: false,
+		progress: "12"
+	},
+	description : "color to add meaning to a button only provides",
+	date : 1402153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40064.jpg",
+	name: "MERCEDESS",
+	id : 6,
+	params: {
+		status: true,
+		progress: "83"
+	},
+	description : "om the content itself (the visible text of the button)",
+	date : 1442153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40063.jpg",
+	name: "SKODA",
+	id : 7,
+	params: {
+		status: true,
+		progress: "49"
+	},
+	description : "r is either obvious from the content itself",
+	date : 1482153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40062.jpg",
+	name: "FORD",
+	id : 8,
+	params: {
+		status: true,
+		progress: "14"
+	},
+	description : "included through alternative means, such as additional text hidden with the",
+	date : 1442153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40059.jpg",
+	name: "TOYOTA",
+	id : 9,
+	params: {
+		status: false,
+		progress: "20"
+	},
+	description : "meaning to a button only provides a visual",
+	date : 1322153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201507/40058.jpg",
+	name: "RENAULT",
+	id : 10,
+	params: {
+		status: true,
+		progress: "40"
+	},
+	description : "uded through alternative means, such as additional",
+	date : 1322159200637
+}]
---
+++ homeWork_17/js/main.js
@@ -0,0 +1,18 @@
+var btn = document.getElementById("play");
+
+function transform() {
+    
+    
+    
+   /*
+   ваша программа 
+   по возможности разбейте на функции
+   */ 
+
+    
+    
+}
+
+
+
+btn.addEventListener("click", transform);
