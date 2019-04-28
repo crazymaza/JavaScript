ddb61a3260a6082ca63d18d852eb302621ee2ec5
crazymaza <dx2389@gmail.com>
2019-04-28 22:56:08 +0300

    Делаю корректировку вывода элементов массива.

--- homeWork_17/js/main.js
+++ homeWork_17/js/main.js
@@ -1,18 +1,45 @@
 var btn = document.getElementById("play");
 
 function transform() {
-    
-    
-    
-   /*
-   ваша программа 
-   по возможности разбейте на функции
-   */ 
-
-    
-    
+        
 }
 
+//Убираем элемент из массива.
+data.splice(5,1)
 
+//Делаю новый массив из старого с отсутствием поля id
+let newArray = []
+data.forEach(item => {
+   newArray.push ({
+      name : item.name,
+      url : item.url,
+      params : item.params,
+      description : item.description,
+      date : item.date,
+   })
+})
+
+//Делаем удобочитаемую дату.
+const newDate = date => {
+   var tmpDate = new Date(date);
+    return tmpDate.getFullYear() + "/" +
+           tmpDate.getMonth() + "/" +
+           tmpDate.getDate() + " " +
+           tmpDate.getHours() + ":" +
+           tmpDate.getMinutes();
+}
+
+//Корректируем вывод элементов.
+const newArrayAfterMap = newArray.map (item => {
+   return {url : `http://${item.url}`,
+         name : `${item.name.charAt(0)}${item.name.slice(1).toLowerCase()}`,
+         description : `${item.description.slice(0,15)}...`,
+         date: newDate(item.date),
+         }
+})
+
+//Закончил на 9-ом задании на странице 
+//https://coursehunter-club.net/t/jsexpert-ponyatnyj-javascript-middle-part-1/809
+console.log(newArrayAfterMap)
 
 btn.addEventListener("click", transform);
