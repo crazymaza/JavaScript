97387766d3a8f0ba16231a049e886c1ee9a116a2
crazymaza <dx2389@gmail.com>
2019-05-04 00:14:12 +0300

    Сделал показ элементов на странице.

--- homeWork_17/js/main.js
+++ homeWork_17/js/main.js
@@ -1,8 +1,7 @@
 var btn = document.getElementById("play");
+let result = document.querySelector("#result")
 
-function transform() {
-        
-}
+
 
 //Убираем элемент из массива.
 data.splice(5,1)
@@ -23,23 +22,48 @@ data.forEach(item => {
 const newDate = date => {
    var tmpDate = new Date(date);
     return tmpDate.getFullYear() + "/" +
-           tmpDate.getMonth() + "/" +
+           (tmpDate.getMonth() + 1) + "/" +
            tmpDate.getDate() + " " +
            tmpDate.getHours() + ":" +
            tmpDate.getMinutes();
 }
 
 //Корректируем вывод элементов.
-const newArrayAfterMap = newArray.map (item => {
+let getNewArrayAfterMap = newArray.map (item => {
    return {url : `http://${item.url}`,
          name : `${item.name.charAt(0)}${item.name.slice(1).toLowerCase()}`,
          description : `${item.description.slice(0,15)}...`,
          date: newDate(item.date),
+         params: `${item.params.status} => ${item.params.progress}`,
+         isVisible: item.params.status,
          }
 })
 
-//Закончил на 9-ом задании на странице 
-//https://coursehunter-club.net/t/jsexpert-ponyatnyj-javascript-middle-part-1/809
-console.log(newArrayAfterMap)
+//Фильтруем массив по значению статуса
+let getNewArrayAfterFilter = getNewArrayAfterMap.filter(item => 
+   item.isVisible === true)
 
+//Закончил на показе коллекции на странице 
+//https://coursehunter-club.net/t/jsexpert-ponyatnyj-javascript-middle-part-2/810
+
+//Вывод на экран элементов массива. Создание элементом с помощью шаблонных строк.
+const createElement = array => { array.forEach(element => {
+      result.insertAdjacentHTML('beforeend',  
+         `<div class = 'wrapper col-lg-4'>
+            <img src = '${element.url}'></img>
+            <h3>${element.name}</h3>
+            <p>${element.description}</p>
+         </div>`
+      )
+   });
+}
+
+//Функция кнопки. Создаю элементы, потом убираю обработчик, затем блокирую кнопку.
+function transform() {
+   createElement(getNewArrayAfterFilter)
+   btn.removeEventListener("click", transform);
+   btn.classList.add('disabled')
+}
+
+//Обработчик клика.
 btn.addEventListener("click", transform);
