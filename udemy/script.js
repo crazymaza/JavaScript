4617eba84947afe3f9cbb254407bc5a173b2cbb4
crazymaza <dx2389@gmail.com>
2019-05-26 15:07:02 +0300

    Сделал первое задание.

---
+++ udemy/script.js
@@ -0,0 +1,19 @@
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let task = prompt("Введите обязательную статью расходов в этом месяце");
+let neededMoney = prompt("Во сколько обойдется?");
+let budget;
+
+let appData = {
+   needMoney: money,
+   timeData: time,
+   expenses: {
+      task: neededMoney,
+   },
+   optionalExpenses: {},
+   income: [],
+   savings: false,
+};
+
+budget = (money - appData.expenses.task) / 30;
+alert("Ваш бюджет на день " + budget.toFixed(2));
