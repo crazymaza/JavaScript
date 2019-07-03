5874db9b3af4a44983edb4236415dda18b0b38cd
crazymaza <dx2389@gmail.com>
2019-07-04 00:06:33 +0300

    сделал конвектор на промисах

---
+++ udemy/Currency/js/script.js
@@ -0,0 +1,55 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+// inputRub.addEventListener('input', () => {
+
+
+
+//     let request = new XMLHttpRequest();
+
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+
+//     request.addEventListener('readystatechange', function () {
+//         if (request.readyState === 4 && request.status == 200) {
+//             let data = JSON.parse(request.response);
+
+//             inputUsd.value = inputRub.value / data.usd;
+//         } else {
+//             inputUsd.value = "Что-то пошло не так!";
+//         }
+//     });
+// });
+
+const startConverter = (elemRUB) => {
+    elemRUB.addEventListener('input', () => {
+        let data;
+        let request = new XMLHttpRequest();
+        const promFunc = () => {
+            return new Promise(function (resolve, reject) {
+                request.open('GET', 'js/current.json');
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+                request.send();
+
+                request.addEventListener('readystatechange', function () {
+                    if (request.readyState === 4) {
+                        if (request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    }
+                });
+                
+            });
+        };
+
+        promFunc(elemRUB)
+                .then(() => data = JSON.parse(request.response))
+                .then(() =>  inputUsd.value = elemRUB.value / data.usd)
+                .catch(() => inputUsd.value = "Что-то пошло не так!");
+    });
+};
+
+startConverter(inputRub);
