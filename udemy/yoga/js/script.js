194de141e511aa28707c41ca19d83a49068f5776
crazymaza <dx2389@gmail.com>
2019-07-07 23:18:11 +0300

    Сделал калькулятор и слайдер

--- udemy/yoga/js/script.js
+++ udemy/yoga/js/script.js
@@ -235,4 +235,108 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     };
     sendForm(popUpForm);
+
+    //Делаем слайдер
+    let slideIndex = 1;
+    let slides = document.querySelectorAll('.slider-item');
+    let prev = document.querySelector('.prev');
+    let next = document.querySelector('.next');
+    let dotsWrap = document.querySelector('.slider-dots');
+    let dots = document.querySelectorAll('.dot');
+
+    //Убираем все слайды со страницы, кроме слайда с нужным индексом.
+    //Также убираем с точек класс active
+    //После чего добавляем нужный слайд(index - номер слайда) и точку на страницу. 
+    const showSlides = index => {
+        if(index > slides.length) {
+            slideIndex = 1;
+        }
+        if(index < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+        dots.forEach(item => item.classList.remove('dot-active'));
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    };
+    showSlides(1); 
+
+    const plusSlides = (index) => {
+        showSlides(slideIndex += index);
+    };
+
+    const curentSlide = (index) => {
+        showSlides(slideIndex = index);
+    };
+
+    //Делаем переключалку на стрелочки.
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    //Делаем переключалку на стрелочки.
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+
+    //Делаем переключалку на кнопки.
+    dotsWrap.addEventListener('click', (event) => {
+        for(let i = 0; i < dots.length + 1; i++) {
+            if(event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                curentSlide(i);
+            }
+        }
+    });
+
+    //Делаем калькулятор
+    let persons = document.querySelectorAll('.counter-block-input')[0];
+    let restDays = document.querySelectorAll('.counter-block-input')[1];
+    let place = document.getElementById('select');
+    let totalValue = document.getElementById('total');
+    let personSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerText = 0;
+
+    persons.addEventListener('change', function() {
+        personSum = +this.value;
+        total = (daysSum + personSum) * 4000;
+
+        if(restDays.value == '' || persons.value == '') {
+            totalValue.innerText = 0;
+        } else {
+            totalValue.innerText = total;
+        }
+    });
+
+    restDays.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = (daysSum + personSum) * 4000;
+
+        if(restDays.value == '' || persons.value == '') {
+            totalValue.innerText = 0;
+        } else {
+            totalValue.innerText = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if(persons.value == '' || restDays.value == '') {
+            totalValue.innerText = 0;
+        } else {
+            let a = total;
+            totalValue.innerText = a * this.options[this.selectedIndex].value;
+        }
+    });
+    
+
+
+
+
+
+
+
+
+
 });
